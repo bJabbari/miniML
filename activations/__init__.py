85e82083b7b422839a1a24ad38db66b4c3bedb88
Biyouk Jabbari <bi.jabbari@gmail.com>
2024-05-01 09:53:54 -0400

    fix:improve activation functions

--- activations/__init__.py
+++ activations/__init__.py
@@ -1,17 +1,21 @@
+
 from miniML.activations.activations import linear
 from miniML.activations.activations import sigmoid
 from miniML.activations.activations import tanh
 from miniML.activations.activations import relu
 from miniML.activations.activations import relu6
+from miniML.activations.activations import leaky_relu
 from miniML.activations.activations import softmax
 
-all_activations = [linear, sigmoid, tanh, relu, relu6, softmax]
+all_activations = [linear, sigmoid, tanh, relu, relu6, leaky_relu, softmax]
 
 all_activations_map = {a.__name__: a for a in all_activations}
 
 
 def get(activation):
-    if isinstance(activation, str):
+    if activation is None:
+        obj = linear
+    elif isinstance(activation, str):
         obj = all_activations_map.get(activation, None)
     else:
         obj = activation
@@ -19,4 +23,4 @@ def get(activation):
     if callable(obj):
         return obj
     else:
-        raise ValueError("input is not an activation function")
+        raise ValueError("activation function is not recognized")
--- activations/activations.py
+++ activations/activations.py
@@ -5,19 +5,23 @@ def linear(x):
     return x
 
 
+@np.vectorize
 def sigmoid(x):
-    return 1.0 / (1 + np.exp(-1 * x))
+    if x >= 0:
+        return 1 / (1 + np.exp(-x))
+    else:
+        z = np.exp(x)
+        return z / (1 + z)
 
 
 def tanh(x):
     return np.tanh(x)
 
 
-def softmax(x: np.ndarray) -> np.ndarray:
-    x_max = np.maximum(x)
-    _x = x - x_max
-    x_exp = np.exp(_x)
-    return x_exp / np.sum(x_exp)
+def softmax(values: np.ndarray, axis=-1) -> np.ndarray:
+    m = np.max(values, axis=axis, keepdims=True)
+    e = np.exp(values - m)
+    return e / np.sum(e, axis=axis, keepdims=True)
 
 
 def relu(x):
@@ -25,4 +29,10 @@ def relu(x):
 
 
 def relu6(x):
-    return np.minimum(relu(x), 6)
+    return np.minimum(np.maximum(0, x), 6)
+
+
+@np.vectorize
+def leaky_relu(x, alpha=0.01):
+    return alpha * x if x <= 0 else x
+
