de95369db36a5d86b3e4ee1ae19841c639fe42ed
Biyouk Jabbari <bi.jabbari@gmail.com>
2024-05-06 10:16:59 -0400

    refactor: Add type hints and minor modification for consistency

--- activations/__init__.py
+++ activations/__init__.py
@@ -1,24 +1,29 @@
+from typing import Union
+
 from .activations import Linear, Sigmoid, Tanh
 from .activations import ReLU, ReLU6, LeakyReLU
 from .activations import Softmax
+from .Activation_function import Activation
 
 all_activations = [Linear, Sigmoid, Tanh, ReLU, ReLU6, LeakyReLU, Softmax]
 all_activations_map = {a.__name__.lower(): a for a in all_activations}
+all_activations_map['leaky_relu']= LeakyReLU
 
 
-def get(activation):
+def get(activation: Union[str, Activation, None]) -> Activation:
     if activation is None:
-        obj = Linear()
+        return Linear()
     elif isinstance(activation, str):
-        obj = all_activations_map.get(activation.lower(), None)
-        if obj is not None:
-            obj = obj()
+        activation_class = all_activations_map.get(activation.lower(), None)
+        if activation_class is not None:
+            activation_instance = activation_class()
+        else:
+            raise ValueError(f"No activation found for '{activation}'.")
     else:
-        obj = activation
+        activation_instance = activation
 
-    if callable(obj):
-        return obj
+    if isinstance(activation_instance, Activation):
+        return activation_instance
     else:
-        raise ValueError("activation function is not recognized")
-
-
+        raise ValueError(f"Invalid activation type. Make sure to pass a valid \'Activation\' subclass instance. "
+                         f"Received: '{type(activation_instance).__name__}'.")
