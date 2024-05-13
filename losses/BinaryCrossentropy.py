94e2e38cc9a961a42fbec21012b16c84672ee689
Biyouk Jabbari <bi.jabbari@gmail.com>
2024-05-13 14:07:11 -0400

    feat: add BinaryCrossentropy loss

---
+++ losses/BinaryCrossentropy.py
@@ -0,0 +1,46 @@
+import numpy as np
+from miniML.losses import Loss
+
+
+class BinaryCrossentropy(Loss):
+    def __init__(self, from_logits=False, eps=1e-7):
+        super().__init__()
+        self.from_logits = from_logits
+        self.eps = eps
+
+    @property
+    def eps(self):
+        return self._eps
+
+    @eps.setter
+    def eps(self, value):
+        if 0 <= value < 1:
+            self._eps = value
+            return
+        raise ValueError(f'eps must be between [0, 1). Received value: {value}')
+
+    def __call__(self, y_true, y_pred):
+        super().__call__(y_true, y_pred)
+        if self.from_logits:
+            self.y_pred = self._sigmoid(self.y_pred)  # convert logits to probability
+            return -np.mean(self.y_true * np.log(self.y_pred) + (1 - self.y_true) * np.log(1 - self.y_pred), axis=None)
+        _y_pred = np.clip(self.y_pred, self.eps, 1.0 - self.eps, dtype=np.float64)
+        return -np.mean(
+            self.y_true * np.log(_y_pred + self.eps) + (1.0 - self.y_true) * np.log(1.0 - _y_pred + self.eps),
+            axis=None)
+
+    def gradient(self):
+        super().gradient()
+        t = (self.y_pred - self.y_true) / self.n_batches
+        if self.from_logits:
+            return t
+        else:
+            epsilon = self.eps  # to avoid division by zero
+            _y_pred = np.clip(self.y_pred, epsilon, 1.0 - epsilon)
+            t = np.where(self.y_true == 0, 1.0 / (1 - _y_pred), -1.0 / _y_pred) / self.n_batches
+
+            return t
+
+    @staticmethod
+    def _sigmoid(x):
+        return 1.0 / (1.0 + np.exp(-x))
--- losses/Loss.py
+++ losses/Loss.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 
 class Loss(ABC):
     def __init__(self):
@@ -7,14 +9,12 @@ class Loss(ABC):
         self.y_pred = None
         self.n_batches = 1
 
-
     @abstractmethod
     def __call__(self, y_true, y_pred):
         if y_true is None or y_pred is None:
             raise ValueError(f"y_true or y_pred can not be None.")
 
-
-        # convert vectors to a two-dimensional matrix, for backpropagation
+        # convert vectors to a two-dimensional matrix, for backpropagation consistency
         if y_true.ndim <= 1:
             self.y_true = y_true.reshape(-1, 1)
         else:
@@ -26,11 +26,15 @@ class Loss(ABC):
 
         if self.y_true.shape != self.y_pred.shape:
             raise ValueError(f"y_true and y_pred must have the same shape.")
+        # Check if dtype is numeric with precision less than float64
+        if np.issubdtype(self.y_pred.dtype, np.floating) and np.finfo(self.y_pred.dtype).bits < 64:
+            # Convert to float64
+            self.y_pred = self.y_pred.astype(np.float64)
         self.n_batches = self.y_pred.shape[0]
+
     @abstractmethod
     def gradient(self):
         if self.y_true is None or self.y_pred is None:
             raise ValueError("y_true or y_pred can not be None.")
         if self.n_batches == 0:
             raise ValueError("batch size cannot be zero. You need to provide at least one sample")
-
