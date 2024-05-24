fix: Correct gradient computation bugs in backpropagation
- Fixed issues with gradient calculation, mostly related to average coefficient in the backpropagation algorithm.

layers/Dense.py
@@ -140,9 +140,6 @@ class Dense(Layer):
 
     def backward(self, gradient: np.ndarray):
         # input gradient.shape should be m * self.units; where m is number of batches
-        n_batches = gradient.shape[0]
-        if gradient.ndim == 0:
-            n_batches = 1
 
         # back propagated gradient through activation function
         grad = self.activations_function.back_propagation(gradient,
@@ -153,12 +150,12 @@ class Dense(Layer):
         propagated_grad = np.matmul(grad, self.weight.T)  # grad.shape is m * self.units
 
         # update weights
-        delta_weight = np.matmul(self.input.T, grad) / float(n_batches)
+        delta_weight = np.matmul(self.input.T, grad)  # / float(n_batches)
         if self._weight_regularize is not None:
             delta_weight += self._weight_regularize.backward(self.weight)
         # self.weight = self.weight - learning_rate * delta_weight
         # update biases
-        delta_bias = np.mean(grad, axis=0, keepdims=True)
+        delta_bias = np.sum(grad, axis=0, keepdims=True)
         if self._bias_regularize is not None:
             delta_bias += self._bias_regularize.backward(self.bias)
         # self.bias = self.bias - learning_rate * delta_bias

losses/BinaryCrossentropy.py
@@ -23,10 +23,12 @@ class BinaryCrossentropy(Loss):
         super().__call__(y_true, y_pred)
         if self.from_logits:
             self.y_pred = self._sigmoid(self.y_pred)  # convert logits to probability
-            return -np.mean(self.y_true * np.log(self.y_pred) + (1 - self.y_true) * np.log(1 - self.y_pred), axis=None)
+            return -np.mean(np.sum(
+                self.y_true * np.log(self.y_pred) + (1 - self.y_true) * np.log(1 - self.y_pred), axis=-1),
+                axis=None)
         _y_pred = np.clip(self.y_pred, self.eps, 1.0 - self.eps, dtype=np.float64)
-        return -np.mean(
-            self.y_true * np.log(_y_pred + self.eps) + (1.0 - self.y_true) * np.log(1.0 - _y_pred + self.eps),
+        return -np.mean(np.sum(
+            self.y_true * np.log(_y_pred + self.eps) + (1.0 - self.y_true) * np.log(1.0 - _y_pred + self.eps), axis=-1),
             axis=None)
 
     def gradient(self):

losses/MeanAbsoluteError.py
@@ -8,8 +8,8 @@ class MeanAbsoluteError(Loss):
 
     def __call__(self, y_true, y_pred):
         super().__call__(y_true, y_pred)
-        return np.mean(np.abs(y_pred - y_true), axis=None)
+        return np.mean(np.sum(np.abs(y_pred - y_true), axis=-1), axis=None)
 
     def gradient(self):
-       super().gradient()
-       return np.sign(self.y_pred - self.y_true) / self.n_batches
+        super().gradient()
+        return np.sign(self.y_pred - self.y_true) / self.n_batches

losses/MeanSquaredError.py
@@ -8,8 +8,8 @@ class MeanSquaredError(Loss):
 
     def __call__(self, y_true, y_pred):
         super().__call__(y_true, y_pred)
-        return np.mean(np.square(y_pred - y_true), axis=None)
+        return np.mean(np.sum(np.square(y_pred - y_true), axis=-1), axis=None)
 
     def gradient(self):
         super().gradient()
-        return (self.y_pred - self.y_true) / 2.0 / self.n_batches
+        return (self.y_pred - self.y_true) * 2.0 / self.n_batches

models/Sequential.py
@@ -184,7 +184,7 @@ class Sequential(Model):
             x_batches = np.array_split(x_shuffled, num_batches)
             y_batches = np.array_split(y_shuffled, num_batches)
         else:
-            x_batches = x_shuffled
-            y_batches = y_shuffled
+            x_batches = [x_shuffled]
+            y_batches = [y_shuffled]
 
         return x_batches, y_batches